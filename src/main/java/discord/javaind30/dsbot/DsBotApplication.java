1d8dc4761ccb99a0f2cf1f3b21a59f86ad2d9d8f
Belousoveu <belousov.eu@gmail.com>
2024-08-01 13:28:14 +0300

    Initial commit

---
+++ src/main/java/discord/javaind30/dsbot/DsBotApplication.java
@@ -0,0 +1,13 @@
+package discord.javaind30.dsbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/discord/javaind30/dsbot/DsbotApplicationTests.java
@@ -0,0 +1,13 @@
+package discord.javaind30.dsbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
